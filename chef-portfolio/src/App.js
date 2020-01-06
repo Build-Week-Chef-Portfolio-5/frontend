0882b1f29b1497a6a10031e7ae8ec7f4775ed79a
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-01-06 15:05:16 -0800

    aeson-johnson

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  const handleChange = event => {
+    setEmail(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form>
+        <label>
+          Email:
+          <input 
+            type="text" 
+            onChange={event => handleChange(event)}
+          />
+        </label>
+      </form>
+
+      <form>
+        <label>
+          Password:
+          <input type="text"/>
+        </label>
+      </form>
+
     </div>
   );
 }
---
+++ chef-portfolio/src/components/Login.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import "./Login.css";
+
+const Login = (props) = > {
+    const [inputValue, setInputValue] = useState("");
+}
+
+
+export default Login;
