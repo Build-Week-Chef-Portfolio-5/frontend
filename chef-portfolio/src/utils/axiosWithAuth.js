f2a17c98c4861fa2ca2d1db88738a57dfee2fe54
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-06 19:40:00 -0500

    added base URL

--- chef-portfolio/src/utils/axiosWithAuth.js
+++ chef-portfolio/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
     return axios.create({
-        baseURL: "",
+        baseURL: "api/chefs",
         headers: {
             Authorization: token
         }
